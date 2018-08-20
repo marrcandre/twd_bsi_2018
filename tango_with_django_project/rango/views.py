97b425d804711d056bd77e8f6dd12a6fd9af7834
Marco André Mendes <marcoandre@gmail.com>
2018-08-20 11:42:36 -0300

    Criação da view about (com url)

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango diz olá pessoal!")
+
+    texto = '''
+    <h1> Rango </h1>
+    Essa é a página principal do Rango
+    
+    <br/><a href='/rango/about/'>Sobre o rango</a>
+    '''
+    return HttpResponse(texto)
+
+
+def about(request):
+    texto = '''
+    <h1> Sobre o Rango </h1>
+    Essa é a página sobre o Rango.
+
+    <br/><a href='/rango/'>Index</a>
+    '''
+    return HttpResponse(texto)
+
