089ce7548ec10d3d9e9b0a37960f3b4c9539f797
Marco André Mendes <marcoandre@gmail.com>
2018-08-30 11:12:27 -0300

    Configuração dos templates e criação do primeiro template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
+    context_dict = {}
+    context_dict['boldmessage'] = "Olá BSI6, este texto veio da view!"
+    context_dict["autor"] = "Marco André Mendes"
 
-    texto = '''
-    <h1> Rango </h1>
-    Essa é a página principal do Rango
-    
-    <br/><a href='/rango/about/'>Sobre o rango</a>
-    '''
-    return HttpResponse(texto)
-
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     texto = '''
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,7 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# print("BASE_DIR:", BASE_DIR)
+# print(__file__)
+# print(os.path.dirname(__file__))
+# print(os.path.dirname(os.path.dirname(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# print("TEMPLATE_DIR:", TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
