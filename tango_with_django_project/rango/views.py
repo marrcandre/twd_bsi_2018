7ce7bf89b412ce00ce5d9ca04e166d14fd1158e0
Marco André Mendes <marcoandre@gmail.com>
2018-09-03 10:51:46 -0300

    Criação do template about.html

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,19 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    context_dict = {}
-    context_dict['boldmessage'] = "Olá BSI6, este texto veio da view!"
-    context_dict["autor"] = "Marco André Mendes"
 
+def index(request):
+    context_dict = {'boldmessage': "Olá BSI6, este texto veio da view!"}
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
-    texto = '''
-    <h1> Sobre o Rango </h1>
-    Essa é a página sobre o Rango.
-
-    <br/><a href='/rango/'>Index</a>
-    '''
-    return HttpResponse(texto)
-
+    context_dict = {'autor': 'Marco André Mendes'}
+    return render(request, 'rango/about.html', context=context_dict)
