280462e549530b2134f09d8b10dae164f6f3e54c
Marco André Mendes <marcoandre@gmail.com>
2018-09-17 10:51:05 -0300

    Exercícios do cap. 6

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,7 +6,10 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     # print('Categorias: ', len(category_list), ':', category_list)
 
-    context_dict = {'categories':category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    print('Páginas: ', len(page_list), ':', page_list)
+
+    context_dict = {'categories':category_list, 'pages':page_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
