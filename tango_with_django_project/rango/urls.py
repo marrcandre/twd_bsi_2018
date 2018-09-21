98f51daad48431e7fe09fef7823bd68a596d1923
Marco André Mendes <marcoandre@gmail.com>
2018-09-21 15:28:54 -0300

    CBV na view Add_Category

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from rango import views
+from .views import Add_Category
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^add_category/', views.add_category, name='add_category'),
+    url(r'^add_category/', Add_Category.as_view(), name='add_category'),
+    # url(r'^add_category/', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
 ]
--- tango_with_django_project/rango/views/add_category.py
+++ tango_with_django_project/rango/views/add_category.py
@@ -1,17 +1,36 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic.base import View
+
 from ..forms import CategoryForm
 
-def add_category(request):
-    form = CategoryForm()
+# def add_category(request):
+#     form = CategoryForm()
+#
+#     if request.method == 'POST':
+#         form = CategoryForm(request.POST)
+#         if form.is_valid():
+#             form.save(commit=True)
+#             return HttpResponseRedirect(reverse('index'))
+#
+#     return render(request, 'rango/add_category.html', {'form': form})
+#     # print(form)
+#     # return HttpResponse('Adcionar Categoria <br/> %s' %form)
 
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
+class Add_Category(View):
+    form_class = CategoryForm
+    initial = {}
+    template_name = 'rango/add_category.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             form.save(commit=True)
             return HttpResponseRedirect(reverse('index'))
 
-    return render(request, 'rango/add_category.html', {'form': form})
-    # print(form)
-    # return HttpResponse('Adcionar Categoria <br/> %s' %form)
+        return render(request, self.template_name, {'form': form})
