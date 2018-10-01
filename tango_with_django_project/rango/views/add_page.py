a568ed5a783c2d460c05d5fd7bae9b172f059c4e
Marco André Mendes <marcoandre@gmail.com>
2018-10-01 13:38:01 -0300

    Forms: extraido método repetido na view de add_page

--- tango_with_django_project/rango/views/add_page.py
+++ tango_with_django_project/rango/views/add_page.py
@@ -5,36 +5,38 @@ from django.views.generic.base import View
 from ..forms import PageForm
 from ..models import Category
 
-class Add_Page(View):
+
+class Add_Page ( View ):
     form_class = PageForm
     initial = {}
     template_name = 'rango/add_page.html'
 
+    def get_Category_or_None(self, category_name_slug):
+        try:
+            category = Category.objects.get ( slug=category_name_slug )
+        except Category.DoesNotExist:
+            category = None
+        return category
+
     def get(self, request, category_name_slug=None):
-        try:
-            category = Category.objects.get(slug=category_name_slug)
-        except Category.DoesNotExist:
-            category = None
+        category = self.get_Category_or_None ( category_name_slug )
 
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form, 'category':category})
+        form = self.form_class ( initial=self.initial )
+        return render ( request, self.template_name, {'form': form, 'category': category} )
 
-    def post(self, request,  category_name_slug=None):
-        try:
-            category = Category.objects.get(slug=category_name_slug)
-        except Category.DoesNotExist:
-            category = None
+    def post(self, request, category_name_slug=None):
+        category = self.get_Category_or_None ( category_name_slug )
 
-        form = self.form_class(request.POST)
-        if form.is_valid():
+        form = self.form_class ( request.POST )
+        if form.is_valid ():
             if category:
-                page = form.save(commit=False)
+                page = form.save ( commit=False )
                 page.category = category
                 page.views = 0
-                page.save()
-                return HttpResponseRedirect(reverse('show_category', args=(category_name_slug,)))
+                page.save ()
+                return HttpResponseRedirect ( reverse ( 'show_category', args=(category_name_slug,) ) )
 
-        return render(request, self.template_name, {'form': form, 'category':category})
+        return render ( request, self.template_name, {'form': form, 'category': category} )
 
 # def add_page(request, category_name_slug):
 #     try:
