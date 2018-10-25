7cdcafd24befbfe653c87d7c30b6deb4fd35ec7f
Marco André Mendes <marcoandre@gmail.com>
2018-10-25 15:47:12 -0300

    Ordenar as páginas por views na listagem de categorias

--- tango_with_django_project/rango/views/show_category.py
+++ tango_with_django_project/rango/views/show_category.py
@@ -6,7 +6,7 @@ def show_category(request, category_name_slug):
     context_dict = {}
     try:
         category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
+        pages = Page.objects.filter(category=category).order_by('-views')
         context_dict['category'] = category
         context_dict['pages'] = pages
     except Category.DoesNotExist:
